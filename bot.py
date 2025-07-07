c085b9a9b68dfafee3b08edefeb7ab9e849bf9d4
capagamamadeverycoolbot <alexahern417@gmail.com>
2025-07-07 22:14:16 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,19 @@
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_BOT_TOKEN")
+
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"✅ Logged in as {bot.user} (ID: {bot.user.id})")
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
+
+bot.run(TOKEN)
